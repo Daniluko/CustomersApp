049449dc8178fa22c3e9884da9d731d2c54d5347
Daniluko <75elehar>
2019-04-03 20:44:26 +0300

    hm

--- src/main/java/springboot/CustomersSpringBootApplication.java
+++ src/main/java/springboot/CustomersSpringBootApplication.java
@@ -2,12 +2,14 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 import java.util.Locale;
 /**
  * Created by Danylo on 31.03.2019
  */
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class CustomersSpringBootApplication {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
